fd55fae28ba55e2b528faccdc6652fd714f993ce
xpj007 <125525181+xpj007@users.noreply.github.com>
2024-05-18 02:00:05 -0300

    :tada: Novo Model Atletas

---
+++ Front_End/GestorCampAngular/src/app/models/atleta.model.ts
@@ -0,0 +1,9 @@
+export class Atleta {
+    id?: any;
+    at_nome?: string;
+    at_dnasc?: Date;
+    at_modalidade?: string;
+    at_nota?: number;
+    at_posicao?: string;
+    publicado?: boolean;
+}
